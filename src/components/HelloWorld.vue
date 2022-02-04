0bdc77711c5436a5d53634be00a6d9e42fbc8bf5
Jirat Chokdeeanan <jirat.cho@gmail.com>
2022-02-04 11:00:05 +0700

    Add poc code

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  mounted() {
+    webview()
+  },
+  data() {
+    return {
+      msg: "Hello"
+    }
+  }
+}
+
+export function webview() {
+  window.webview = {
+      getBackButtonClickEvent : (data) => {
+        console.log(JSON.parse(data))
+        const clicked = JSON.parse(data).clicked
+        if (clicked) {     
+          alert('BACK')
+          console.log('Do some go back')
+         //backPage()
+        } else {
+          console.log('Do noting', data)
+        }
+        return 'getBackButtonClickEvent data received'
+      }
+  }
+}
+</script>
